84e6b8ef347a212b079caaad716d90c84bc5f23a
Daniel <d.peruffo@gmail.com>
2026-06-05 16:22:42 -0300

    Add start script for running webhook server and Streamlit

---
+++ estudo-de-rede/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python webhook_server.py &
+streamlit run estudo_de_rede.py --server.port $PORT --server.address 0.0.0.0 --server.headless true --server.enableCORS false --server.enableXsrfProtection true
