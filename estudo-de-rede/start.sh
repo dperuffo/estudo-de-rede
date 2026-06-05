9b35f8115ac50333b28c9c715f09f0ade5171d12
Daniel <d.peruffo@gmail.com>
2026-06-05 16:54:29 -0300

    Delete estudo-de-rede/start.sh

--- estudo-de-rede/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python webhook_server.py &
-streamlit run estudo_de_rede.py --server.port $PORT --server.address 0.0.0.0 --server.headless true --server.enableCORS false --server.enableXsrfProtection true
