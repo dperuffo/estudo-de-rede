c721c7f437592f07ce9296a13db5e191c44088bb
Daniel <d.peruffo@gmail.com>
2026-06-26 08:57:26 -0300

    feat: Flutter flutter/lib/core/constants/api_constants.dart

---
+++ flutter/lib/core/constants/api_constants.dart
@@ -0,0 +1,12 @@
+class ApiConstants {
+  static const String baseUrl = "https://api.fxgestaodefrotasonline.com";
+  static const String authGoogle = "/auth/google";
+  static const String authRefresh = "/auth/refresh";
+  static const String authMe = "/auth/me";
+  static const String abastecimentos = "/abastecimentos";
+  static const String abastecimentosResumo = "/abastecimentos/resumo";
+  static const String frotaVeiculos = "/frota/veiculos";
+  static const String manutencao = "/manutencao";
+  static const String tickets = "/tickets";
+  static const String financeiroResumo = "/financeiro/resumo";
+}
