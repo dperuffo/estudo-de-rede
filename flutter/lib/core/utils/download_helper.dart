a2141fd25645fa5234df235b4bc45d38c7405cbd
Daniel Peruffo <d.peruffo@gmail.com>
2026-06-29 09:03:08 -0300

    fix: download anexo via JavaScript nativo

--- flutter/lib/core/utils/download_helper.dart
+++ flutter/lib/core/utils/download_helper.dart
@@ -1,13 +1,5 @@
-import 'dart:convert';
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'dart:js' as js;
 
 void downloadBase64File(String base64str, String nome, String mime) {
-  final bytes = base64Decode(base64str);
-  final blob = html.Blob([bytes], mime);
-  final url = html.Url.createObjectUrlFromBlob(blob);
-  final anchor = html.AnchorElement(href: url)
-    ..setAttribute('download', nome)
-    ..click();
-  html.Url.revokeObjectUrl(url);
+  js.context.callMethod('downloadBase64', [base64str, nome, mime]);
 }
