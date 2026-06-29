d446aa189346d0efa15fdc9b8616ce394fe9efba
Daniel Peruffo <d.peruffo@gmail.com>
2026-06-29 09:12:03 -0300

    fix: download anexo usando dart:js_interop

--- flutter/lib/core/utils/download_helper.dart
+++ flutter/lib/core/utils/download_helper.dart
@@ -1,5 +1,11 @@
-import 'dart:js' as js;
+@JS()
+library download_helper;
+
+import 'dart:js_interop';
+
+@JS('downloadBase64')
+external void _downloadBase64JS(String base64, String filename, String mime);
 
 void downloadBase64File(String base64str, String nome, String mime) {
-  js.context.callMethod('downloadBase64', [base64str, nome, mime]);
+  _downloadBase64JS(base64str, nome, mime);
 }
