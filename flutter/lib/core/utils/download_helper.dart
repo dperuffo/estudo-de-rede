a74ceb8b722e32fa163949dffb1435db02f7829e
Daniel Peruffo <d.peruffo@gmail.com>
2026-06-29 09:28:53 -0300

    fix: download anexo usando dart:html direto

--- flutter/lib/core/utils/download_helper.dart
+++ flutter/lib/core/utils/download_helper.dart
@@ -1,11 +1,18 @@
-@JS()
-library download_helper;
-
-import 'dart:js_interop';
-
-@JS('downloadBase64')
-external void _downloadBase64JS(String base64, String filename, String mime);
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+import 'dart:convert';
 
 void downloadBase64File(String base64str, String nome, String mime) {
-  _downloadBase64JS(base64str, nome, mime);
+  final bytes = base64Decode(base64str);
+  final blob = html.Blob([bytes], mime);
+  final url = html.Url.createObjectUrlFromBlob(blob);
+  // ignore: unsafe_html
+  final anchor = html.document.createElement('a') as html.AnchorElement;
+  anchor.href = url;
+  anchor.download = nome;
+  anchor.style.display = 'none';
+  html.document.body!.append(anchor);
+  anchor.click();
+  anchor.remove();
+  html.Url.revokeObjectUrl(url);
 }
